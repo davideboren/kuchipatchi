7c0df01f58a8320666c3fc2868069948d5e7ca73
davideboren <davideboren@gmail.com>
2018-06-03 14:47:21 -0500

    Revert "Revert "ActiveMonsterDB functional""
    This reverts commit fbb852a237bf03c90517e0ab4ff7a8853fa0fe09.

--- ActiveMonsterDB.h
+++ ActiveMonsterDB.h
@@ -8,7 +8,9 @@ struct ActiveMonsterDB{
   std::vector<Monster*> monsters_p;
 
   ActiveMonsterDB(MonsterDB mondb);
+  void addMonster(int id);
   void addMover(int id);
   Monster * getMonster(int pos);
+  int numActiveMonsters();
 };
 
