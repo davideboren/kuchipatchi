348edf186adb325ca5050ff1e12462606f84a8a0
davideboren <davideboren@gmail.com>
2018-06-29 13:29:11 -0500

    fixed indentation on flusher

--- FlusherFX.ino
+++ FlusherFX.ino
@@ -11,7 +11,7 @@ FlusherFX::FlusherFX(){
 void FlusherFX::heartbeat(){
 	if(flusherPos < 32){
 		fx[0].setFrame(spr,128-flusherPos*12,0,-1);
-    fx[1].setFrame(spr,128-flusherPos*12,32,-1);
+    		fx[1].setFrame(spr,128-flusherPos*12,32,-1);
 		flusherPos++;
 	} else {
 		animDone = true;
